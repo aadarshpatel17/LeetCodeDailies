25fb12900c553cf3830e590e21d1e815e23aa63e
Aadarsh <kaadarsh173@gmail.com>
2026-01-07 11:31:41 +0530

    freaking DSA done: 1 leetcode daily and 4 questions for fun!!!

---
+++ src/main/java/org/practise/dsa_training/day7/TwoSum_1.java
@@ -0,0 +1,22 @@
+package org.practise.dsa_training.day7;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class TwoSum_1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int right = 0; right<nums.length; right++) {
+            int currTarget = target - nums[right];
+            if(map.containsKey(currTarget)) {
+                return new int[] {right, map.get(currTarget)};
+            }
+            map.put(nums[right], right);
+        }
+        return new int[] {-1};
+    }
+
+}
