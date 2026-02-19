d0a2cf3db38efc06defe0a4d16cd47e64f6d2351
Aadarsh <kaadarsh173@gmail.com>
2026-02-19 08:59:10 +0530

    commiting old practise project

--- src/main/java/org/practise/Main.java
+++ src/main/java/org/practise/Main.java
@@ -1,17 +1,29 @@
 package org.practise;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(reverseStringII("train"));
+
+    }
+
+    public static String reverseString(String str ){
+        return Stream.of(str)
+                .map(word -> new StringBuilder(word).reverse())
+                .collect(Collectors.joining());
+    }
+
+    public static String reverseStringII(String str ) {
+        return IntStream.range(0, str.length())
+                .map(i -> str.charAt(str.length() - i - 1))
+                .collect(StringBuilder::new,
+                        (sb, c) -> sb.append((char) c),
+                        StringBuilder::append).toString();
     }
 }
